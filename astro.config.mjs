15a7cb3b9855aaec31ce17c6ab9312249a90f175
Piccoli Augusto <piccoli_44@hotmail.com>
2025-05-11 12:43:17 -0300

    corrigiendo errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
-  adapter: netlify (),
+  output: 'static', 
   integrations: [tailwind(), robotsTxt()],
   site: 'https://piccoliaugusto.com.ar/',
-  i18n: {
-    locales: ['en', 'es'], 
-    defaultLocale: 'es',  
-  },
 })
