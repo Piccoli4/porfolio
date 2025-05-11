da0519717b1909ba0ff72c0517c42ad9890207f2
Piccoli Augusto <piccoli_44@hotmail.com>
2025-05-11 12:44:33 -0300

    problemas con deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static', 
+  adapter: netlify (),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://piccoliaugusto.com.ar/',
 })
